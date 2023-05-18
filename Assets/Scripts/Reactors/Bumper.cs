3f041d4e9fe82711aa3853164516f5d3a9fd6665
Conner A <eraserlark@gmail.com>
2023-05-18 13:51:54 -0500

    Bumper tweaks

--- Assets/Scripts/Reactors/Bumper.cs
+++ Assets/Scripts/Reactors/Bumper.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class Bumper : CollideReactor
 {
+    [SerializeField]
+    private float upwardForce = 0.5f;
+    [SerializeField]
+    private float bounceForce = 500f;
+
     public override void CollEnter(Collision collision)
     {
         Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
         if(rb != null)
         {
             Vector3 normal = -collision.GetContact(0).normal;
-            Vector3 bounceHeight = Vector3.up * 0.5f;   //bounceUp = 0.5f;
-            Vector3 bounceVector = (normal + bounceHeight) * 500f * rb.mass;  //bounceForce = 500f;
+            Vector3 bounceHeight = Vector3.up * upwardForce;
+            Vector3 bounceVector = (normal + bounceHeight) * bounceForce;
 
-            rb.AddForce(bounceVector);
+            rb.AddForce(bounceVector, ForceMode.Acceleration);
         }
     }
 }
