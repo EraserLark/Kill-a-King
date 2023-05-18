b76631cf9f4a901885a3b35778089e517e0257cf
Conner A <eraserlark@gmail.com>
2023-05-18 09:15:13 -0500

    Flipper refactor

---
+++ Assets/Scripts/Reactors/Flipper.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Flipper : TriggerReactor
+{
+    private BoxCollider triggerColl;
+
+    private HingeJoint joint;
+    private JointMotor motor;
+
+    [SerializeField]
+    private float motorForce = 2500f;
+    [SerializeField]
+    private float motorVel = 900f;
+    [SerializeField]
+    private bool motorFreeSpin = false;
+
+    private void Awake()
+    {
+        triggerColl = GetComponents<BoxCollider>()[1];  //Get trigger collider, not other boxColl
+
+        joint = GetComponent<HingeJoint>();
+        motor = joint.motor;
+
+        motor.force = motorForce;
+        motor.targetVelocity = motorVel;
+        motor.freeSpin = motorFreeSpin;
+        joint.motor = motor;
+    }
+
+    public override void TrigEnter(Collider other)
+    {
+        motor.targetVelocity = motorVel;
+        joint.motor = motor;
+
+        joint.useMotor = true;
+        triggerColl.enabled = false;
+
+        Invoke("ResetPaddle", 1f);
+    }
+
+    private void ResetPaddle()
+    {
+        motor.targetVelocity = -motorVel;
+        joint.motor = motor;
+        //joint.useMotor = true;
+
+        triggerColl.enabled = true;
+    }
+}
